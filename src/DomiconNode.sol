7caf518296d651d0a3c3bcf4b6fbaa8e2405f333
Yulin <yulin970210@gmail.com>
2024-05-27 12:07:12 +0800

    ops:add Ownable

--- src/DomiconNode.sol
+++ src/DomiconNode.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { Initializable } from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import { ISemver } from "src/universal/ISemver.sol";
 
-contract DomiconNode is Initializable, ISemver {
+contract DomiconNode is Initializable, ISemver,Ownable {
 
     using SafeERC20 for IERC20;
 
@@ -41,24 +42,24 @@ contract DomiconNode is Initializable, ISemver {
 
     /// @notice Initializer
     function initialize() public initializer {
-
+        _transferOwnership(tx.origin);
     }
 
     function RegisterBroadcastNode(NodeInfo calldata info) external {
         require(info.addr==tx.origin);
-        IERC20(DOM).safeTransferFrom(msg.sender, address(this), info.stakedTokens);
+//        IERC20(DOM).safeTransferFrom(msg.sender, address(this), info.stakedTokens);
 
         broadcastNodeList.push(info.addr);
         broadcastingNodes[info.addr] = info;
         emit BroadcastNode(info.addr,info.url,info.name,info.stakedTokens);
     }
 
-    function RegisterStorageNodeList(NodeInfo calldata info) external {
-        IERC20(DOM).safeTransferFrom(msg.sender, address(this), info.stakedTokens);
+    function RegisterStorageNode(NodeInfo calldata info) external {
+//        IERC20(DOM).safeTransferFrom(msg.sender, address(this), info.stakedTokens);
 
         storageNodeList.push(info.addr);
         storageNodes[info.addr] = info;
-        emit BroadcastNode(info.addr,info.url,info.name,info.stakedTokens);
+        emit StorageNode(info.addr,info.url,info.name,info.stakedTokens);
     }
 
     function IsNodeBroadcast(address addr) external view returns (bool){
@@ -68,7 +69,7 @@ contract DomiconNode is Initializable, ISemver {
         return false;
     }
 
-    function SetDom(address addr)external {
+    function SetDom(address addr) external onlyOwner {
         DOM = addr;
     }
 }
