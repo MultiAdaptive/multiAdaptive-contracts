e743fac6cbe7d68408bf0714fbd338de86ca47ff
Yulin <yulin970210@gmail.com>
2024-06-03 11:46:54 +0800

    ops:upd IMPL_SALT

--- script/Config.sol
+++ script/Config.sol
@@ -45,7 +45,7 @@ library Config {
 
     /// @notice The CREATE2 salt to be used when deploying the implementations.
     function implSalt() internal view returns (string memory _env) {
-        _env = vm.envOr("IMPL_SALT", string("ether 0527C"));
+        _env = vm.envOr("IMPL_SALT", string("multiAdaptive"));
     }
 
     /// @notice Returns the path that the state dump file should be written to or read from
