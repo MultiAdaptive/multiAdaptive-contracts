be69f564a5fcd184599bcd88dc9e455e5f9192fd
Yulin <yulin970210@gmail.com>
2024-06-11 16:23:55 +0800

    ops:upd SRS

--- src/kzg/Verifier.sol
+++ src/kzg/Verifier.sol
@@ -86,25 +86,6 @@ contract Verifier is Constants {
     }
 
     /*
-     * @return A KZG commitment to a polynominal
-     * @param coefficients The coefficients of the polynomial to which to
-     *                     commit.
-     */
-    function commit(uint256[] memory coefficients) public view returns (Pairing.G1Point memory) {
-        Pairing.G1Point memory result = Pairing.G1Point(0, 0);
-
-        for (uint256 i = 0; i < coefficients.length; i++) {
-            result = Pairing.plus(
-                result,
-                Pairing.mulScalar(
-                    Pairing.G1Point({ X: Constants.SRS_G1_X[i], Y: Constants.SRS_G1_Y[i] }), coefficients[i]
-                )
-            );
-        }
-        return result;
-    }
-
-    /*
      * @return The polynominal evaluation of a polynominal with the specified
      *         coefficients at the given index.
      */
@@ -121,78 +102,6 @@ contract Verifier is Constants {
             }
         }
         return result;
-    }
-
-    /*
-     * Verifies the evaluation of multiple points of a polynominal using the
-     * KZG commitment scheme.
-     *    - p(X) is a polynominal
-     *    - commitment = commit(p)
-     *    - For each y in _values and each x in _indices, y = p(x)
-     *    - proof = genMultiProof(p, _indices)
-     * Returns true if and only if the following holds, and returns false
-     * otherwise:
-     *     e(-commit(zPoly), proof) * e(commitment - commit(iPoly), g) == 1
-     * @param _commitment The polynominal commitment.
-     * @param _proof The proof.
-     * @param _indices The x-values at which to evaluate the polynominal.
-     * @param _values The evaluation of the polynominal at each index.
-     * @param _iCoeffs The coefficients of a polynominal which interpolates
-     *                 each index and corresponding y-value.
-     * @param _zCoeffs The coefficients of a polynominal which intersects y=0
-     *                 for each index.
-     */
-    function verifyMulti(
-        Pairing.G1Point memory _commitment,
-        Pairing.G2Point memory _proof,
-        uint256[] memory _indices,
-        uint256[] memory _values,
-        uint256[] memory _iCoeffs,
-        uint256[] memory _zCoeffs
-    )
-        public
-        view
-        returns (bool)
-    {
-        // Perform range checks
-        require(_commitment.X < BABYJUB_P, "Verifier.verifyMultiKZG: _commitment.X is out of range");
-        require(_commitment.Y < BABYJUB_P, "Verifier.verifyMultiKZG: _commitment.Y is out of range");
-        require(_proof.X[0] < BABYJUB_P, "Verifier.verifyKZG: _proof.X[0] is out of range");
-        require(_proof.X[1] < BABYJUB_P, "Verifier.verifyKZG: _proof.X[1] is out of range");
-        require(_proof.Y[0] < BABYJUB_P, "Verifier.verifyKZG: _proof.Y[0] is out of range");
-        require(_proof.Y[1] < BABYJUB_P, "Verifier.verifyKZG: _proof.Y[1] is out of range");
-
-        for (uint256 i = 0; i < _iCoeffs.length; i++) {
-            require(_iCoeffs[i] < BABYJUB_P, "Verifier.verifyKZG: an _iCoeffs value is out of range");
-        }
-
-        for (uint256 i = 0; i < _zCoeffs.length; i++) {
-            require(_zCoeffs[i] < BABYJUB_P, "Verifier.verifyKZG: an _zCoeffs value is out of range");
-        }
-
-        // Check whether _iCoeffs and _zCoeffs are valid
-        for (uint256 i = 0; i < _indices.length; i++) {
-            uint256 index = _indices[i];
-            uint256 value = _values[i];
-            require(index < BABYJUB_P, "Verifier.verifyKZG: an index is out of range");
-            require(value < BABYJUB_P, "Verifier.verifyKZG: a value is out of range");
-
-            uint256 zEval = evalPolyAt(_zCoeffs, _indices[i]);
-            require(zEval == 0, "Verifier.verifyMulti: invalid _zCoeffs");
-
-            uint256 iEval = evalPolyAt(_iCoeffs, _indices[i]);
-            require(iEval == _values[i], "Verifier.verifyMulti: invalid _iCoeffs");
-        }
-
-        // Generate the KZG commitments to the i and z polynominals
-        Pairing.G1Point memory zCommit = commit(_zCoeffs);
-        Pairing.G1Point memory iCommit = commit(_iCoeffs);
-
-        // Compute commitment - commit(iPoly)
-        Pairing.G1Point memory commitmentMinusICommit = Pairing.plus(_commitment, Pairing.negate(iCommit));
-
-        // Perform the pairing check
-        return Pairing.pairing(Pairing.negate(zCommit), _proof, commitmentMinusICommit, g2Generator);
     }
 
     /*
