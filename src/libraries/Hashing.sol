e50b2af113e205ecf7585f3ff8a5274044c0e307
Yulin <yulin970210@gmail.com>
2024-07-04 14:08:30 +0800

    ops:upd

--- src/libraries/Hashing.sol
+++ src/libraries/Hashing.sol
@@ -8,23 +8,8 @@ library Hashing {
         return keccak256(abi.encode(_x, _y));
     }
 
-    /// @notice Used for computing the overall commitment state hash $r_i=H(cm_i||addr_i||r_{i-1})$
-    function hashCommitmentRoot(
-        uint256 _x,
-        uint256 _y,
-        address _target,
-        bytes32 _committeeHash
-    )
-        internal
-        pure
-        returns (bytes32)
-    {
-        return keccak256(abi.encode(_x, _y, _target, _committeeHash));
-    }
-
     /// @notice Used for generating a hash corresponding to a set of broadcast node addresses
     function hashAddresses(
-        address _sender,
         uint256 _requiredAmountOfSignatures,
         address[] memory _addresses
     )
@@ -32,7 +17,11 @@ library Hashing {
         pure
         returns (bytes32)
     {
-        return keccak256(abi.encode(_sender, _requiredAmountOfSignatures, _addresses));
+        return keccak256(abi.encode(_requiredAmountOfSignatures, _addresses));
+    }
+
+    function hashAddresses(address _creator, address[] memory _addresses) internal pure returns (bytes32) {
+        return keccak256(abi.encode(_creator, _addresses));
     }
 
     function hashSignatures(bytes[] calldata _signatures) internal pure returns (bytes32) {
@@ -44,6 +33,7 @@ library Hashing {
         address _target,
         uint256 _index,
         uint256 _length,
+        uint256 _timeout,
         uint256 _x,
         uint256 _y
     )
@@ -55,28 +45,11 @@ library Hashing {
         assembly {
             _chainId := chainid()
         }
-        bytes memory data = abi.encode(_chainId, _target, _index, _length, _x, _y);
+        bytes memory data = abi.encode(_chainId, _target, _index, _length, _timeout, _x, _y);
         return keccak256(data);
     }
 
     function hashFold(uint256 _r, uint256 _n) internal pure returns (bytes32) {
         return keccak256(abi.encode(_r, _n));
-    }
-
-    /// @notice Verify Signature.
-    function verifySignature(bytes32 _dataHash, bytes memory signature, address _sender) internal pure returns (bool) {
-        require(signature.length == 65, "Invalid signature length");
-        uint8 v;
-        bytes32 r;
-        bytes32 s;
-        assembly {
-            r := mload(add(signature, 32))
-            s := mload(add(signature, 64))
-            v := byte(0, mload(add(signature, 96)))
-        }
-
-        address signer = ecrecover(_dataHash, v, r, s);
-        require(signer != address(0), "address is not avaible");
-        return signer == _sender;
     }
 }
