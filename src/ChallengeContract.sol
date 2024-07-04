9b9db2de2836ae142352dc9a6c27d01bfd81c555
Yulin <yulin970210@gmail.com>
2024-07-04 14:15:20 +0800

    ops:namespaceID becomes nameSpaceKey

--- src/ChallengeContract.sol
+++ src/ChallengeContract.sol
@@ -7,129 +7,184 @@ import { ISemver } from "src/universal/ISemver.sol";
 import { NodeManager } from "src/NodeManager.sol";
 import { CommitmentManager } from "src/CommitmentManager.sol";
 import { Hashing } from "src/libraries/Hashing.sol";
+import { ChallengeStatus } from "src/libraries/ChallengeStatus.sol";
 import { Verifier } from "src/kzg/Verifier.sol";
 import { Pairing } from "src/kzg/Pairing.sol";
 
-struct Challenge {
-    uint256 nonce;
-    uint8 status;
-    address challenger;
-    address storageAddr;
-    uint256 nameSpaceId;
-    uint256 start;
-    uint256 end;
-    uint256 r;
-    Pairing.G1Point aggregateCommitment;
-    uint256 point;
-    uint256 timeoutBlock;
-}
+/// @title ChallengeContract
+/// @notice This contract manages challenges related to storage commitments.
+contract ChallengeContract is ChallengeStatus, Initializable, ISemver, Ownable {
+    /// @notice Stores information about a challenge.
+    struct Challenge {
+        uint256 nonce;
+        uint8 status;
+        address challenger;
+        address storageAddr;
+        bytes32 nameSpaceKey;
+        uint256 start;
+        uint256 end;
+        uint256 r;
+        Pairing.G1Point aggregateCommitment;
+        uint256 point;
+        uint256 timeoutBlock;
+    }
 
-struct ChallengeDetatils {
-    uint256 consensusIndex;
-    uint256 noConsensusIndex;
-    uint256 currentIndex;
-    Pairing.G1Point consensusCommitment;
-    Pairing.G1Point noConsensusCommitment;
-    Pairing.G1Point currAggregateCommitment;
-}
+    /// @notice Stores detailed information about a challenge's status.
+    struct ChallengeDetatils {
+        uint256 consensusIndex;
+        uint256 noConsensusIndex;
+        uint256 currentIndex;
+        Pairing.G1Point consensusCommitment;
+        Pairing.G1Point noConsensusCommitment;
+        Pairing.G1Point currAggregateCommitment;
+    }
 
-contract ChallengeContract is Initializable, ISemver, Ownable {
     /// @notice Semantic version.
     /// @custom:semver 0.1.0
     string public constant version = "0.1.0";
 
+    /// @notice Instance of the NodeManager contract.
     NodeManager public nodeManager;
 
+    /// @notice Instance of the CommitmentManager contract.
     CommitmentManager public commitmentManager;
 
+    /// @notice Instance of the kzg verifier contract.
     Verifier public verifier;
 
+    /// @notice Mapping to store challenges by their ID.
     mapping(uint256 => Challenge) public challenges;
 
+    /// @notice Mapping to store detailed information about each challenge.
     mapping(uint256 => ChallengeDetatils) public challengeDetailsMap;
 
+    /// @notice Counter for challenge IDs.
     uint256 public nonce;
 
+    /// @notice Event emitted when a new challenge is created.
     event ChallengeCreated(
         uint256 nonce,
         address storageAddr,
-        uint256 nameSpaceId,
+        bytes32 nameSpaceKey,
         uint256 start,
         uint256 end,
         uint256 r,
         uint256 timeoutBlock
     );
 
-    event AggregateCommitmentUploaded(uint256 nonce, Pairing.G1Point aggregateCommitment, uint256 timeoutBlock);
+    /// @notice Event emitted when an aggregate commitment is submit.
+    event AggregateCommitmentSubmit(uint256 nonce, Pairing.G1Point aggregateCommitment, uint256 timeoutBlock);
 
     /// @notice Constructs the ChallengeContract contract.
     constructor() { }
 
-    /// @notice Initializer
+    /// @notice Initializer function to set up the contract.
+    /// @param _nodeManager Instance of the NodeManager contract.
+    /// @param _commitmentManager Instance of the CommitmentManager contract.
     function initialize(NodeManager _nodeManager, CommitmentManager _commitmentManager) public initializer {
         nodeManager = _nodeManager;
         commitmentManager = _commitmentManager;
         _transferOwnership(tx.origin);
     }
 
+    /// @notice Creates a new challenge.
+    /// @param _start Start index for the challenge.
+    /// @param _end End index for the challenge.
+    /// @param _storageAddr Address of the storage node being challenged.
+    /// @param _r Random value used in the challenge.
+    /// @param _point Point for the challenge.
+    /// @param _nameSpaceKey Namespace key associated with the challenge.
     function createChallenge(
         uint256 _start,
         uint256 _end,
         address _storageAddr,
-        uint256 _nameSpaceId,
         uint256 _r,
-        uint256 _point
+        uint256 _point,
+        bytes32 _nameSpaceKey
     )
-        public
+        external
         payable
     {
-        //        require(msg.value > 0, "ChallengeContract: minimum entry fee required");
+        // require(msg.value > 0, "ChallengeContract: minimum entry fee required");
 
         Challenge storage challenge = challenges[nonce];
 
         challenge.nonce = nonce;
-        challenge.status = 0;
+        challenge.status = CHALLENGE_CREATED;
         challenge.challenger = msg.sender;
         challenge.storageAddr = _storageAddr;
-        challenge.nameSpaceId = _nameSpaceId;
+        challenge.nameSpaceKey = _nameSpaceKey;
         challenge.start = _start;
         challenge.end = _end;
         challenge.r = _r;
         challenge.point = _point;
         challenge.timeoutBlock = block.number + 600;
 
-        emit ChallengeCreated(nonce, _storageAddr, _nameSpaceId, _start, _end, _r, challenge.timeoutBlock);
+        emit ChallengeCreated(nonce, _storageAddr, _nameSpaceKey, _start, _end, _r, challenge.timeoutBlock);
 
         nonce++;
     }
 
-    function uploadAggregateCommitment(uint256 _challengeId, Pairing.G1Point calldata _commitment) public {
+    /// @notice Submits an aggregate commitment for a challenge.
+    /// @param _challengeId ID of the challenge.
+    /// @param _commitment The aggregate commitment point.
+    function submitAggregateCommitment(uint256 _challengeId, Pairing.G1Point calldata _commitment) external {
         Challenge storage challenge = challenges[_challengeId];
 
-        require(challenge.status == 0 || challenge.status == 3, "ChallengeContract: challenge is already complete");
+        require(
+            challenge.status == CHALLENGE_CREATED || challenge.status == COMMIT_NOT_AGREED,
+            "ChallengeContract: challenge is already complete"
+        );
 
         require(challenge.timeoutBlock >= block.number, "ChallengeContract: timed out");
 
         require(msg.sender == challenge.storageAddr, "ChallengeContract: only the storage node can upload commitment");
 
-        if (challenge.status == 0) {
+        if (challenge.status == CHALLENGE_CREATED) {
             challenge.aggregateCommitment = _commitment;
-            challenge.status = 1;
+            challenge.status = FIRST_COMMIT_SUBMITTED;
         } else {
             ChallengeDetatils storage details = challengeDetailsMap[challenge.nonce];
             details.currAggregateCommitment = _commitment;
-            challenge.status = 2;
+            challenge.status = RECOMMIT_SUBMITTED;
         }
 
-        emit AggregateCommitmentUploaded(_challengeId, _commitment, challenge.timeoutBlock);
+        emit AggregateCommitmentSubmit(_challengeId, _commitment, challenge.timeoutBlock);
 
         challenge.timeoutBlock = block.number + 600;
     }
 
-    function uploadProof(uint256 _challengeId, Pairing.G1Point calldata _proof, uint256 _value) public {
+    /// @notice Submits an opinion on the aggregate commitment.
+    /// @param _challengeId ID of the challenge.
+    /// @param _agreed Boolean indicating if the opinion agrees with the commitment.
+    function submitOpinion(uint256 _challengeId, bool _agreed) external {
         Challenge storage challenge = challenges[_challengeId];
 
-        require(challenge.status == 5, "ChallengeContract: consensus on commitment not reached");
+        require(challenge.challenger == msg.sender, "ChallengeContract: only the challenger can submit an opinion");
+
+        require(
+            challenge.status == FIRST_COMMIT_SUBMITTED || challenge.status == RECOMMIT_SUBMITTED,
+            "ChallengeContract: unsubmitted aggregate commitment"
+        );
+
+        require(challenge.timeoutBlock >= block.number, "ChallengeContract: timed out");
+
+        if (challenge.status == FIRST_COMMIT_SUBMITTED) {
+            _handleInitialStatus(challenge, _agreed);
+        } else {
+            _handleOngoingStatus(challenge, _agreed);
+        }
+        challenge.timeoutBlock = block.number + 600;
+    }
+
+    /// @notice Submits a proof for a challenge.
+    /// @param _challengeId ID of the challenge.
+    /// @param _proof The proof point.
+    /// @param _value The value to be proven.
+    function uploadProof(uint256 _challengeId, Pairing.G1Point calldata _proof, uint256 _value) external {
+        Challenge storage challenge = challenges[_challengeId];
+
+        require(challenge.status == AGREEMENT_REACHED, "ChallengeContract: consensus on commitment not reached");
 
         require(challenge.timeoutBlock >= block.number, "ChallengeContract: timed out");
 
@@ -137,74 +192,83 @@ contract ChallengeContract is Initializable, ISemver, Ownable {
 
         bool isValid = verifyAggregateCommitment(challenge.aggregateCommitment, _proof, challenge.point, _value);
 
-        challenge.status = isValid ? 8 : 7;
+        challenge.status = isValid ? CHALLENGE_SUCCESSFUL : CHALLENGE_FAILED;
     }
 
-    function submitOpinion(uint256 _challengeId, bool _agreed) public {
-        Challenge storage challenge = challenges[_challengeId];
-
-        require(challenge.challenger == msg.sender, "ChallengeContract: only the challenger can submit an opinion");
-
-        require(challenge.status == 1 || challenge.status == 2, "ChallengeContract: unsubmitted aggregate commitment");
-
-        require(challenge.timeoutBlock >= block.number, "ChallengeContract: timed out");
-
-        if (challenge.status == 1) {
-            handleInitialStatus(challenge, _agreed);
-        } else {
-            handleOngoingStatus(challenge, _agreed);
-        }
-        challenge.timeoutBlock = block.number + 600;
+    /// @notice Withdraws the contract balance to the owner's address.
+    function withdraw() external onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "StorageManager: No balance to withdraw");
+        payable(msg.sender).transfer(balance);
     }
 
-    function handleInitialStatus(Challenge storage challenge, bool _agreed) internal {
+    /// @notice Sets the address of the KZG verifier contract.
+    /// @param _addr Address of the verifier contract.
+    function setKZG(address _addr) external onlyOwner {
+        verifier = Verifier(_addr);
+    }
+
+    /// @notice Handles the initial opinion submission.
+    /// @param challenge The challenge being addressed.
+    /// @param _agreed Boolean indicating if the opinion agrees with the commitment.
+    function _handleInitialStatus(Challenge storage challenge, bool _agreed) internal {
         if (_agreed) {
-            challenge.status = 5;
+            challenge.status = AGREEMENT_REACHED;
         } else {
             ChallengeDetatils storage details = challengeDetailsMap[challenge.nonce];
             details.consensusIndex = challenge.start;
             details.noConsensusIndex = challenge.end;
             details.noConsensusCommitment = challenge.aggregateCommitment;
             details.currentIndex = (challenge.start + challenge.end) / 2;
-            challenge.status = 3;
+            challenge.status = COMMIT_NOT_AGREED;
         }
     }
 
-    function handleOngoingStatus(Challenge storage challenge, bool _agreed) internal {
+    /// @notice Handles ongoing opinion submission.
+    /// @param challenge The challenge being addressed.
+    /// @param _agreed Boolean indicating if the opinion agrees with the commitment.
+    function _handleOngoingStatus(Challenge storage challenge, bool _agreed) internal {
         ChallengeDetatils storage details = challengeDetailsMap[challenge.nonce];
 
         if (_agreed) {
             details.consensusIndex = details.currentIndex;
             details.consensusCommitment = details.currAggregateCommitment;
-            challenge.status = 4;
+            challenge.status = TEMPORARY_AGREEMENT;
         } else {
             details.noConsensusIndex = details.currentIndex;
             details.noConsensusCommitment = details.currAggregateCommitment;
-            challenge.status = 3;
+            challenge.status = COMMIT_NOT_AGREED;
         }
 
         if (details.consensusIndex == details.noConsensusIndex - 1) {
             if (details.consensusIndex == challenge.start) {
-                details.consensusCommitment = aggregateCommitment(challenge.start, challenge.nameSpaceId, challenge.r);
+                details.consensusCommitment = aggregateCommitment(challenge.start, challenge.nameSpaceKey, challenge.r);
             }
             bool isValid = verifyAggregateCommitment(
                 details.consensusCommitment,
                 details.noConsensusCommitment,
                 details.noConsensusIndex,
-                challenge.nameSpaceId,
+                challenge.nameSpaceKey,
                 challenge.r
             );
-            challenge.status = isValid ? 8 : 7;
+            challenge.status = isValid ? CHALLENGE_SUCCESSFUL : CHALLENGE_FAILED;
         }
 
         details.currentIndex = (details.noConsensusIndex + details.consensusIndex) / 2;
     }
 
+    /// @notice Verifies an aggregate commitment.
+    /// @param _consensusCommitment Commitment point where consensus was reached.
+    /// @param _aggregateCommitment Aggregate commitment point.
+    /// @param _index Index being evaluated.
+    /// @param _nameSpaceKey Namespace key associated with the challenge.
+    /// @param _r Random value used in the challenge.
+    /// @return Boolean indicating if the aggregate commitment is valid.
     function verifyAggregateCommitment(
         Pairing.G1Point memory _consensusCommitment,
         Pairing.G1Point memory _aggregateCommitment,
         uint256 _index,
-        uint256 _nameSpaceId,
+        bytes32 _nameSpaceKey,
         uint256 _r
     )
         public
@@ -212,11 +276,17 @@ contract ChallengeContract is Initializable, ISemver, Ownable {
         returns (bool)
     {
         bytes32 hash = Hashing.hashFold(_r, _index);
-        Pairing.G1Point memory n0 = Pairing.mulScalar(commitment(_index, _nameSpaceId), uint256(hash));
+        Pairing.G1Point memory n0 = Pairing.mulScalar(commitment(_index, _nameSpaceKey), uint256(hash));
         Pairing.G1Point memory n1 = Pairing.plus(_consensusCommitment, n0);
         return Pairing.equal(_aggregateCommitment, n1);
     }
 
+    /// @notice Verifies a commitment proof.
+    /// @param _commitment Commitment point.
+    /// @param _proof Proof point.
+    /// @param _index Index of the commitment.
+    /// @param _value Value to be proven.
+    /// @return Boolean indicating if the proof is valid.
     function verifyAggregateCommitment(
         Pairing.G1Point memory _commitment,
         Pairing.G1Point memory _proof,
@@ -230,31 +300,30 @@ contract ChallengeContract is Initializable, ISemver, Ownable {
         return verifier.verify(_commitment, _proof, _index, _value);
     }
 
+    /// @notice Computes the aggregate commitment for a given index, namespace key, and random value.
+    /// @param _index Index being evaluated.
+    /// @param _nameSpaceKey Namespace key associated with the challenge.
+    /// @param _r Random value used in the challenge.
+    /// @return Aggregate commitment point.
     function aggregateCommitment(
         uint256 _index,
-        uint256 _nameSpaceId,
+        bytes32 _nameSpaceKey,
         uint256 _r
     )
         public
         view
         returns (Pairing.G1Point memory)
     {
-        Pairing.G1Point memory n0 = commitment(_index, _nameSpaceId);
+        Pairing.G1Point memory n0 = commitment(_index, _nameSpaceKey);
         bytes32 hash = Hashing.hashFold(_r, _index);
         return Pairing.mulScalar(n0, uint256(hash));
     }
 
-    function withdraw() external onlyOwner {
-        uint256 balance = address(this).balance;
-        require(balance > 0, "StorageManager: No balance to withdraw");
-        payable(msg.sender).transfer(balance);
-    }
-
-    function commitment(uint256 _index, uint256 _nameSpaceId) public view returns (Pairing.G1Point memory) {
-        return commitmentManager.getNameSpaceCommitment(_nameSpaceId, _index);
-    }
-
-    function SetKZG(address _addr) external onlyOwner {
-        verifier = Verifier(_addr);
+    /// @notice Gets the commitment for a given index and namespace key.
+    /// @param _index Index being evaluated.
+    /// @param _nameSpaceKey Namespace key associated with the challenge.
+    /// @return Commitment point.
+    function commitment(uint256 _index, bytes32 _nameSpaceKey) public view returns (Pairing.G1Point memory) {
+        return commitmentManager.getNameSpaceCommitment(_nameSpaceKey, _index);
     }
 }
